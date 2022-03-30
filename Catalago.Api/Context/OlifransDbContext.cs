6f43a03395dfc77504fda7dd01339152433436ae
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-30 10:30:36 -0300

    feat: Class DbContext

---
+++ Catalago.Api/Context/OlifransDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Catalago.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalago.Api.Context
+{
+    public class OlifransDbContext : DbContext
+    {
+        public OlifransDbContext(DbContextOptions<OlifransDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
