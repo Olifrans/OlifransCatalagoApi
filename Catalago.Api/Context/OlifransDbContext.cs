b6b4a225af7ce6f1971dded8eedff5074cca3676
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-30 10:52:26 -0300

    feat: Fluent-API --> Reforçando e explicitando, configurando, mapeando propriedades, tipos e relacionamentos

--- Catalago.Api/Context/OlifransDbContext.cs
+++ Catalago.Api/Context/OlifransDbContext.cs
@@ -11,5 +11,27 @@ namespace Catalago.Api.Context
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+
+        // Fluent-API--> Reforçando e explicitando, configurando, mapeando propriedades, tipos e relacionamentos
+        protected override void OnModelCreating(ModelBuilder mbOptions)
+        {
+            //Categoria
+            mbOptions.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mbOptions.Entity<Categoria>().Property(c => c.Nome).HasMaxLength(100).IsRequired();
+            mbOptions.Entity<Categoria>().Property(c => c.Descricao).HasMaxLength(150).IsRequired();
+
+            //Produto
+            mbOptions.Entity<Produto>().HasKey(p => p.ProdutoId);
+            mbOptions.Entity<Produto>().Property(p => p.Nome).HasMaxLength(100).IsRequired();
+            mbOptions.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(150).IsRequired();
+            mbOptions.Entity<Produto>().Property(p => p.Imagem).HasMaxLength(150);
+            mbOptions.Entity<Produto>().Property(p => p.Preco).HasPrecision(14, 2);
+
+            //Relacionamentos
+            mbOptions.Entity<Produto>()
+                .HasOne<Categoria>(c => c.Categoria)
+                .WithMany(p => p.Produtos)
+                .HasForeignKey(c => c.CategoriaId);
+        }
     }
 }
--- Catalago.Api/Models/Categoria.cs
+++ Catalago.Api/Models/Categoria.cs
@@ -8,7 +8,7 @@ namespace Catalago.Api.Models
 {
     public class Categoria
     {
-        public int Id { get; set; }
+        public int CategoriaId { get; set; }
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
 
