ae385a1fa7c5085d5a58f426f0db7ec992486a34
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-04-01 10:15:32 -0300

    Feat: Coclusão minimal Api NetCore06

--- Catalago.Api/Program.cs
+++ Catalago.Api/Program.cs
@@ -1,22 +1,27 @@
 using Catalago.Api.ApiEndpoints;
 using Catalago.Api.AppServicesExtensions;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container --> Semelhante ao ConfigureServices da class Startup net5
-
-//Swagger teste API
 builder.AddSwagger();
 builder.AddPersistence();
 builder.Services.AddCors();
 builder.AddAuthenticationJwt();
 
+
+
 var app = builder.Build();
+
 
 //---------------------Endpoints
 app.MapAutenticacaoEndpoints();
 app.MapCategoriaEndpoints();
 app.MapProdutoEndpoints();
+
+
 
 // Configure the HTTP request pipeline --> Semelhante ao Configure da class Startup net5
 var environment = app.Environment;
